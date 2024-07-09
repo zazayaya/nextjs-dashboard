c73a07ed8a4e00ce52cc731e9d1ac2cd5bf3f525
zaza <260458726@qq.com>
2024-07-09 17:19:37 +0800

    Chapter 15

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -13,7 +13,6 @@ import { createInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
 
-
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
